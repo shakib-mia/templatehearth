863eaf3653dd3467e9d728aa1f6d6003652038f5
shakib-mia <smdshakibmia2001@gmail.com>
2025-11-21 09:30:50 +0600

    refactor: simplify Templates component and improve static parameter generation

--- src/app/components/Templates/Templates.js
+++ src/app/components/Templates/Templates.js
@@ -1,40 +1,26 @@
-import React from "react";
-import { db } from "@/app/lib/mongodb";
-import Template from "../Template/Template";
+export const revalidate = 60; // ISR enabled
 
-// -------------------------
-// 1) IF THERE ARE DYNAMIC TEMPLATE PAGES THIS WILL PRE-GENERATE THEM
-// -------------------------
+import Template from "../Template/Template";
+import { db } from "@/app/lib/mongodb";
+
 export async function generateStaticParams() {
-  const { slug: type } = await params;
+  const templatesCollection = db.collection("templates");
+  const types = await templatesCollection.distinct("type");
+
+  return types.map((type) => ({ type }));
+}
+
+export default async function TemplatesPage({ params }) {
+  const { type } = params;
+
   const templatesCollection = db.collection("templates");
   const templates = await templatesCollection.find({ type }).toArray();
 
-  return templates.map((t) => ({
-    slug: t.slug, // jodi tui dynamic template pages generate korte chas
-  }));
-}
-
-// -------------------------
-// 2) MAIN COMPONENT (SSG STYLE)
-// -------------------------
-const Templates = async ({ route, params }) => {
-  // console.log({ params });
-  // const { slug: type } = await params;
-  const type = params ? { type: params } : {};
-
-  const templatesCollection = db.collection("templates");
-  const templates = await templatesCollection.find(type).toArray();
-
-  const displayedTemplates = route === "/" ? templates.slice(0, 4) : templates;
-
   return (
     <div className="col-span-5 lg:col-span-4 grid grid-cols-1 lg:grid-cols-2 gap-4">
-      {displayedTemplates.map((template, key) => (
-        <Template key={key} {...template} />
+      {templates.map((template) => (
+        <Template key={template.slug} {...template} />
       ))}
     </div>
   );
-};
-
-export default Templates;
+}
