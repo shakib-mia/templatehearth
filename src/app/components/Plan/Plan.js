120d15e99deed696c51862bcd1cd1821bb12902a
shakib-mia <smdshakibmia2001@gmail.com>
2025-11-26 09:46:38 +0600

    fix: display tagline in Plan component header

--- src/app/components/Plan/Plan.js
+++ src/app/components/Plan/Plan.js
@@ -18,7 +18,7 @@ const Plan = ({
       {/* Header */}
       <div className="mb-4">
         <h3 className="text-lg font-medium text-gray-900 ">{name}</h3>
-        <p className="text-sm text-gray-500 ">{}</p>
+        <p className="text-sm text-gray-500 ">{tagline}</p>
       </div>
 
       {/* Price */}
